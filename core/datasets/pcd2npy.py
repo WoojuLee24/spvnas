7fefe3abd8ce0ac16a95f65ab31b7a55323e6d39
woojulee24 <bigtree12343@gmail.com>
2022-05-02 19:34:43 +0900

    convert pcd to npy file

--- core/datasets/pcd2npy.py
+++ core/datasets/pcd2npy.py
@@ -28,35 +28,37 @@ seqs = ['scenario2', 'scenario3', 'scenario5', 'scenario6', 'scenario8']
 # seqs = ['scenario3', 'scenario5', 'scenario6', 'scenario8']
 root_path = "/ws/data/erasor_carla/carla_dataset/"
 
-# save map npy data
-for seq in seqs:
-    print(seq)
-    map_path = os.path.join(root_path, "testing_map/v0.05", seq, "map.pcd")
-    new_map_path = os.path.splitext(map_path)[0] + ".npy"
-    points = read_pc(map_path)
-    np.save(new_map_path, points)
-
-# # save scan npy data
+# # save map npy data
 # for seq in seqs:
-#     scan_dir = os.path.join(root_path, "testing_data", seq, "global/scan/")
-#     new_scan_dir = os.path.join(root_path, "testing_data", seq, "global_npz")
-#     odom_path = os.path.join(root_path, "testing_data", seq, "odom/scan/odometry.txt")
-#     odoms = get_pose(odom_path)
-#     if not os.path.exists(new_scan_dir):
-#         os.makedirs(new_scan_dir)
-#         # os.makedirs(os.path.join(new_scan_dir, "scan"))
-#         # new_scan_dir = os.path.join(new_scan_dir, "scan")
-#     scan_filenames = os.listdir(scan_dir)
-#     scan_files = [os.path.join(scan_dir, x) for x in scan_filenames]
-#     dummy = scan_files
-#     for idx, scan_filename in enumerate(scan_filenames):
-#         scan_file = os.path.join(scan_dir, scan_filename)
-#         points = read_pc(scan_file)
-#         odom = odoms[idx]
-#         new_scan_file = os.path.join(new_scan_dir, scan_filename[:-4].zfill(6))
-#         np.savez(new_scan_file, points, odom)
-#         # np.save(new_scan_file, points)
+#     print(seq)
+#     map_path = os.path.join(root_path, "testing_map/v0.1", seq, "map.pcd")
+#     new_map_path = os.path.splitext(map_path)[0] + ".npy"
+#     points = read_pc(map_path)
+#     np.save(new_map_path, points)
 
+# save scan npy data
+for seq in seqs:
+    scan_dir = os.path.join(root_path, "testing_data", seq, "global/scan/")
+    new_scan_dir = os.path.join(root_path, "testing_data", seq, "global_npz")
+    odom_path = os.path.join(root_path, "testing_data", seq, "odom/scan/odometry.txt")
+    odoms = get_pose(odom_path)
+    if not os.path.exists(new_scan_dir):
+        os.makedirs(new_scan_dir)
+        # os.makedirs(os.path.join(new_scan_dir, "scan"))
+        # new_scan_dir = os.path.join(new_scan_dir, "scan")
+    scan_filenames = sorted(os.listdir(scan_dir))
+    scan_files = [os.path.join(scan_dir, x) for x in scan_filenames]
+    dummy = scan_files
+    for idx, scan_filename in enumerate(scan_filenames):
+        scan_file = os.path.join(scan_dir, scan_filename)
+        points = read_pc(scan_file)
+        odom = odoms[idx]
+        new_scan_file = os.path.join(new_scan_dir, scan_filename[:-4])
+        np.savez(new_scan_file, points, odom)
+        # np.save(new_scan_file, points)
+
+
+### deprecated
 # # save map pose npy
 # for seq in seqs:
 #     odom_path = os.path.join("/ws/data/erasor_carla/carla_dataset/testing_data", seq, "odom/map/odometry.txt")
